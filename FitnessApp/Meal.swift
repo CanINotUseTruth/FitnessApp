6bcc7054f758e936d6e38a91856332df1de7960d
sonnyly1 <79506972+sonnyly1@users.noreply.github.com>
2023-05-27 04:46:25 +1000

    added meal tracker view controllers

---
+++ FitnessApp/Meal.swift
@@ -0,0 +1,39 @@
+//
+//  Meal.swift
+//  FitnessApp
+//
+//  Created by Sonny on 27/5/2023.
+//
+
+//import Foundation
+//
+//struct Meal {
+//    var name: String
+//    var food: [Food]?
+//
+////    static func == (lhs: Meal, rhs: Meal) -> Bool {
+////        return lhs.name == rhs.name
+////    }
+////    func hash(into hasher: inout Hasher) {
+////        hasher.combine(name)
+////    }
+//}
+//
+//struct MealHeaders {
+//    var type: String
+//}
+//
+////
+////  Meal.swift
+////  FitnessApp
+////
+////  Created by Sonny on 27/5/2023.
+////
+
+import Foundation
+
+struct Meal {
+    var category: String
+    var name: String
+    var calories: String?
+}
