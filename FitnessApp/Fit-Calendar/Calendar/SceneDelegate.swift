ae9fb7dda4a6501747f7df3967e8290b28a0022a
CanINotUseTruth <tobyrutherfordfb@gmail.com>
2023-05-29 21:04:48 +1000

    Connected Calendar
    Connected calendar feature to main storyboard and cleaned up directories.

--- FitnessApp/Fit-Calendar/Calendar/SceneDelegate.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var window: UIWindow?
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    }
-}
